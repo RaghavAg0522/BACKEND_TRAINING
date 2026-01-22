d1d12c5dc2376446c2477875d4ff16195f72bd46
Raghav <raghavagrawal0505@gmail.com>
2026-01-22 23:41:17 +0530

    Added the solution of QUESTION-02

---
+++ BACKEND_TRAINING/question2/question2.js
@@ -0,0 +1,5 @@
+const { capitalize, reverseString, countVowels } = require("./stringUtils.js");
+
+console.log(capitalize("hello"));      // Hello
+console.log(reverseString("world"));   // dlrow
+console.log(countVowels("JavaScript"));// 3
